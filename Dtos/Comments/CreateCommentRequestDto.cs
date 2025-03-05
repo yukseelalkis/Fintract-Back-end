8000389cbaa681fcb93d0a5b3c416ff30238e813
Yuksel <alkisyuksel7@gmail.com>
2025-03-05 16:03:50 +0300

    Geçici commit: Değişiklikler kaydedildi

--- Dtos/Comments/CreateCommentRequestDto.cs
+++ Dtos/Comments/CreateCommentRequestDto.cs
@@ -8,6 +8,9 @@ namespace api.Dtos.Comments
     public class CreateCommentRequestDto
     {
         // idsi olmicak 
+
+        // Burada bir validate islem yapmamiz lazimsa
+        
         public string Title { get; set; } = string.Empty;
 
         public string  Content { get; set; } = string.Empty;
