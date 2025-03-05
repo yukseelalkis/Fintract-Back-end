74672c6fb2f0b5ca8210554ee71197dd809d7d41
Yuksel <alkisyuksel7@gmail.com>
2025-03-05 17:29:36 +0300

    Added validation rules to CreateCommentRequestDto

--- Dtos/Comments/CreateCommentRequestDto.cs
+++ Dtos/Comments/CreateCommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,16 @@ namespace api.Dtos.Comments
     {
         // idsi olmicak 
 
-        // Burada bir validate islem yapmamiz lazimsa
-        
+        // BUrada yorumun en az 5 karekter icermesini istiyorsak DOGRULAMA ISLEMI ICIN GEREKLI
+        // Dogrulama mesajlari cok tekrara dusuyorr buna bakilacak!!!!!!!!!!
+        [Required]
+        [MinLength(5,ErrorMessage ="Title  must  be 5 characters")]
+        [MaxLength(280 , ErrorMessage ="Title cannot be over  280 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5,ErrorMessage ="Content  must  be 5 characters")]
+        [MaxLength(280 , ErrorMessage ="Title cannot be over  280 characters")]
 
         public string  Content { get; set; } = string.Empty;
 
